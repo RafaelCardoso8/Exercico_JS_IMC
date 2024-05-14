0a2eb4f8ec076b8796ee46a6412fb1f89d1be974
Rafael Cardoso <faelcardoso8@gmail.com>
2024-05-13 22:15:00 -0300

    3

--- calculoimc.js
+++ calculoimc.js
@@ -1,4 +1,5 @@
-
+const resultado_imc = document.getElementById('res_IMC')
+const resultado_clas = document.getElementById('res_classificacao')
 
 function calculoimc() {
     let peso = document.getElementById('peso')
@@ -8,5 +9,29 @@ function calculoimc() {
 
     let imc = p / (a * a)
     console.log('funcionou', a, p , imc)
+    resultado_imc.innerHTML = (`O IMC é ${imc}.`)
+
+    if ( imc < 17 ){
+        resultado_clas.innerHTML= (`Muito abaixo do peso`)
+    }
+    else if (imc < 18.5){
+        resultado_clas.innerHTML= (`Abaixo do peso`)
+    }
+    else if ( imc < 25){
+        resultado_clas.innerHTML= (`Peso normal`)
+    }
+    else if (imc < 30){
+        resultado_clas.innerHTML= (`Acima do peso`)
+    }
+    else if (imc < 35){
+        resultado_clas.innerHTML= (`Obesidade grau 1`)
+    }
+    else if( imc < 40){
+        resultado_clas.innerHTML= (`Obesidade grau 2 `)
+    }
+    else{
+        resultado_clas.innerHTML= (`Obesidade grau 3 `)
+    }
+
 }
 
